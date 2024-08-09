6e4f1eea6e697da6113cfdf1d312edc1de8ec239
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-08-09 20:00:46 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const users = require("./sample.json");
 const cors = require("cors");
-const fs = require("fs")
+const fs = require("fs");
 
 const app = express();
 app.use(express.json());
@@ -9,7 +9,7 @@ const port = 8000;
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://crud-application-f7.vercel.app", // Updated URL
         methods: ["GET", "POST", "PATCH", "DELETE"],
     })
 );
@@ -19,45 +19,54 @@ app.get("/users", (req, res) => {
     return res.json(users);
 });
 
-//Delete User Details
-app.delete("/users/:id", (req,res) => {
+// Delete User Details
+app.delete("/users/:id", (req, res) => {
     let id = Number(req.params.id);
-    let filteredUsers=users.filter((user)=>user.id !== id);
-    fs.writeFile("./sample.json",JSON.stringify(filteredUsers),(err,data)=>{
+    let filteredUsers = users.filter((user) => user.id !== id);
+    fs.writeFile("./sample.json", JSON.stringify(filteredUsers), (err) => {
+        if (err) {
+            return res.status(500).send({ message: "Error deleting user" });
+        }
         return res.json(filteredUsers);
     });
 });
 
-//Add New User
-app.post("/users",(req, res) => {
-    let { name, age, profession} = req.body;
-    if(!name || !age || !profession){
-        res.status(400).send({message: "All Fields Required"})
+// Add New User
+app.post("/users", (req, res) => {
+    let { name, age, profession } = req.body;
+    if (!name || !age || !profession) {
+        return res.status(400).send({ message: "All Fields Required" });
     }
     let id = Date.now();
-    users.push({id, name, age, profession});
-    fs.writeFile("./sample.json",JSON.stringify(filteredUsers),(err,data)=>{
-    return res.json({"message": "User Details Added Successfully"});
+    users.push({ id, name, age, profession });
+    fs.writeFile("./sample.json", JSON.stringify(users), (err) => {
+        if (err) {
+            return res.status(500).send({ message: "Error adding user" });
+        }
+        return res.json({ message: "User Details Added Successfully" });
     });
 });
 
-//Update User
-app.patch("/users/:id",(req, res) => {
+// Update User
+app.patch("/users/:id", (req, res) => {
     let id = Number(req.params.id);
-    let { name, age, profession} = req.body;
-    if(!name || !age || !profession){
-        res.status(400).send({message: "All Fields Required"})
+    let { name, age, profession } = req.body;
+    if (!name || !age || !profession) {
+        return res.status(400).send({ message: "All Fields Required" });
     }
-    
     let index = users.findIndex((user) => user.id == id);
-
-    users.splice(index,1, {...req.body});
-
-    fs.writeFile("./sample.json",JSON.stringify(users),(err,data)=>{
-    return res.json({"message": "User Details Updated!"});
-    });
+    if (index !== -1) {
+        users[index] = { ...req.body, id }; // Keep the same ID
+        fs.writeFile("./sample.json", JSON.stringify(users), (err) => {
+            if (err) {
+                return res.status(500).send({ message: "Error updating user" });
+            }
+            return res.json({ message: "User Details Updated!" });
+        });
+    } else {
+        return res.status(404).send({ message: "User not found" });
+    }
 });
-
 
 app.listen(port, (err) => {
     if (err) {
